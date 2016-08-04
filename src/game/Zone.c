b46b7690b1091d1a862f6ae064dbc2389dae8de1
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2016-08-04 16:51:09 -0400

    Returned to normal.

--- src/game/Zone.c
+++ src/game/Zone.c
@@ -73,17 +73,15 @@ void Zone(const struct AutoSpaceZone *const sz) {
 	/* update the current zone */
 	current_zone = sz;
 
-#if 1
 	/* some asteroids */
-	for(i = 0; i < 7000; i++) {
+	for(i = 0; i < /*7000*/1000; i++) {
 		s = Sprite(SP_DEBRIS, AutoImageSearch("Asteroid.png"), (int)rnd(de_sitter), (int)rnd(de_sitter), rnd((float)M_PI), big_asteroid_mass);
 		SpriteSetVelocity(s, rnd(0.02f), rnd(0.02f));
 		SpriteSetOmega(s, rnd(10.0f));
 	}
-#endif
 
 	/* sprinkle some ships */
-	for(i = 0; i < 200; i++) {
+	for(i = 0; i < /*200*/50; i++) {
 		Sprite(SP_SHIP, (int)rnd(de_sitter), (int)rnd(de_sitter), rnd((float)M_PI), scorpion_class, B_STUPID);
 	}
 

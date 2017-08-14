c079303ac3d896776b9142ce95c5bac210e9a629
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2017-08-14 15:07:26 -0400

    Aha! Delay is getting two entries, and the first entry is for delete, making the second entry invalid.

--- src/game/Sprite.c
+++ src/game/Sprite.c
@@ -12,7 +12,9 @@
  @std		C89/90
  @version	3.4, 2017-05 generics
  @since		3.3, 2016-01
-			3.2, 2015-06 */
+			3.2, 2015-06
+ @fixme Still crashes.
+ @fixme Collision resolution wonky. */
 
 #include <stdlib.h>	/* rand */
 #include <stdio.h>  /* fprintf */
@@ -71,11 +73,10 @@ static float dt_ms;
 
 /** {Sprite} virtual table. */
 struct SpriteVt;
-/* {Sprite} temporary on-screen values.
- struct SpriteTemp; ?? is it worth it? */
 /** Collisions between two sprites, cleared at every frame. */
 struct Collision;
-/** Define {Sprite}. */
+/** Define {Sprite}. This is, if you will, abstract: {SpriteListNode}, derived
+ from {Sprite}, is always part of a bigger structure. {vt} is polymorphic. */
 struct Sprite {
 	const struct SpriteVt *vt; /* virtual table pointer */
 	const struct AutoImage *image, *normals; /* what the sprite is */
@@ -84,6 +85,7 @@ struct Sprite {
 	struct Ortho3f x, v; /* where it is and where it is going */
 	struct Vec2f dx, x_5; /* temporary values */
 	float bounding, bounding1; /* bounding1 is temporary */
+	/*struct Rectangle4f box;*/ /* fixme: temp bounding box; tighter bounds */
 };
 /** @implements <Bin>Comparator
  @fixme Actually, we can get rid of this. Unused? That was a lot of work for
@@ -109,8 +111,7 @@ static void Sprite_to_string(const struct Sprite *this, char (*const a)[12]);
 /** Every sprite has one {bin} based on their position. \see{OrthoMath.h}. */
 static struct SpriteList bins[BIN_BIN_FG_SIZE + 1];
 static struct SpriteList *const holding_bin = bins + BIN_BIN_FG_SIZE;
-/** Fills a sprite with data. This is kind of an abstract class: it never
- appears outside of another struct.
+/** Fills a sprite with data. It never appears outside of another struct.
  @param auto: An auto-sprite prototype, contained in the precompiled {Auto.h}
  resources, that specifies graphics.
  @param vt: Virtual table.
@@ -369,8 +370,13 @@ struct Delay {
 	SpriteAction action;
 	struct Sprite *sprite;
 };
+static void Delay_to_string(const struct Delay *this, char (*const a)[12]) {
+	assert(this);
+	Sprite_to_string(this->sprite, a);
+}
 #define SET_NAME Delay
 #define SET_TYPE struct Delay
+#define SET_TO_STRING &Delay_to_string
 #include "../general/Set.h" /* defines TransferSet, TransferSetNode */
 static struct DelaySet *delays;
 
@@ -1083,6 +1089,7 @@ void SpritesUpdate(const int dt_ms_passed, struct Ship *const player) {
 	/* Calculate the future positions and transfer the sprites that have
 	 escaped their bin or been deleted. */
 	for_each_update(&extrapolate);
+	printf("Delay: %s.\n", DelaySetToString(delays));
 	DelaySetForEach(delays, &Delay_evaluate), DelaySetClear(delays);
 	/* fixme: place things in to drawing area. */
 	/* Collision relies on the values calculated in \see{extrapolate}. */
@@ -1273,85 +1280,6 @@ void SpritesOut(void) {
 
 #if 0
 
-/* Define class {Sprite}. */
-
-struct SpriteVt;
-struct Sprite {
-	const struct SpriteVt *vt;
-	unsigned bin;
-	struct Ortho3f r, v; /* position, orientation, velocity, angular velocity */
-	struct Vec2f r1, v1; /* temp position on the next frame */
-	struct Rectangle4f box; /* temp bounding box */
-	float t0_dt_collide; /* temp time to collide over frame time */
-	unsigned no_collisions; /* temp number of collisions */
-	float mass; /* t (ie Mg) */
-	float bounding;
-	unsigned image, normals; /* gpu texture refs */
-	struct Vec2u size;
-};
-/** @implements <Sprite>Comparator */
-static int Sprite_x_cmp(const struct Sprite *a, const struct Sprite *b) {
-	return (b->r.x < a->r.x) - (a->r.x < b->r.x);
-}
-/** @implements <Sprite>Comparator */
-static int Sprite_y_cmp(const struct Sprite *a, const struct Sprite *b) {
-	return (b->r.x < a->r.x) - (a->r.x < b->r.x);
-}
-/** {vt} is left undefined; this is, if you will, an abstract struct (haha.)
- Initialises {this} with {AutoSprite} resource, {as}. */
-static void Sprite_filler(struct Sprite *const this,
-	const struct AutoSprite *const as, const unsigned bin,
-	struct Ortho3f *const r, const struct Ortho3f *const v) {
-	assert(this);
-	assert(as);
-	assert(as->image->texture);
-	assert(as->normals->texture);
-	assert(bin < BIN_BIN_SIZE);
-	assert(r);
-	this->vt = 0;
-	this->bin = bin;
-	Ortho3f_clip_position(r);
-	Ortho3f_assign(&this->r, r);
-	Ortho3f_assign(&this->v, v);
-	this->r1.x = r->x;
-	this->r1.y = r->y;
-	this->v1.x = v->x;
-	this->v1.y = v->y;
-	Rectangle4f_init(&this->box);
-	this->t0_dt_collide = 0.0f;
-	this->no_collisions = 0;
-	this->mass = 1.0f;
-	this->bounding = as->image->width * 0.5f;
-	this->image = as->image->texture;
-	this->normals = as->normals->texture;
-	this->size.x = as->image->width;
-	this->size.y = as->image->height;
-}
-/* Awkward declarations for {List.h}. */
-struct ListMigrate;
-struct SpriteList;
-/* Define {SpriteList} and {SpriteListNode}. */
-#define LIST_NAME Sprite
-#define LIST_TYPE struct Sprite
-#define LIST_UA_NAME X
-#define LIST_UA_COMPARATOR &Sprite_x_cmp
-#define LIST_UB_NAME Y
-#define LIST_UB_COMPARATOR &Sprite_y_cmp
-#include "../general/List.h"
-/** Every sprite has one {bin} based on their position. \see{OrthoMath.h}. */
-static struct SpriteList sprites[BIN_BIN_SIZE];
-
-/** Define {Debris} as a subclass of {Sprite}. */
-struct Debris {
-	struct SpriteListNode sprite;
-	int hit;
-	/*void (*on_kill)(void); minerals? */
-};
-/* Define DebrisSet. */
-#define SET_NAME Debris
-#define SET_TYPE struct Debris
-#include "../general/Set.h"
-
 /** Define {Ship} as a subclass of {Sprite}. */
 struct Ship {
 	struct SpriteListNode sprite;
@@ -1386,89 +1314,6 @@ struct Wmd {
 #define SET_NAME Wmd
 #define SET_TYPE struct Wmd
 #include "../general/Set.h"
-
-/** Define {Gate} as a subclass of {Sprite}. */
-struct Gate {
-	struct SpriteListNode sprite;
-	const struct AutoSpaceZone *to;
-};
-/* Define GateSet. */
-#define SET_NAME Gate
-#define SET_TYPE struct Gate
-#include "../general/Set.h"
-
-/* Storage; the actual data goes here. */
-struct DebrisSet *debris;
-struct ShipSet *ships;
-struct WmdSet *wmds;
-struct GateSet *gates;
-
-/** Changes the window parameters so all of them don't point to {this} and
- deletes {this} from the sprite list.
- @implements <Sprite>Action */
-static void sprite_remove(struct Sprite *const this) {
-	/* fixme: window parameters */
-	char a[12];
-	assert(this);
-	Sprite_to_string(this, &a);
-	printf("Sprite_delete %s#%p.\n", a, (void *)this);
-	SpriteListRemove(sprites + this->bin, this);
-}
-
-/** @implements <Debris>Action */
-static void debris_out(struct Debris *const this) {
-	assert(this);
-	printf("Debris at (%f, %f) in %u integrity %u.\n", this->sprite.data.r.x,
-		this->sprite.data.r.y, this->sprite.data.bin, this->hit);
-}
-/** @implements <Debris>Action */
-static void debris_delete(struct Debris *const this) {
-	char a[12];
-	assert(this);
-	Debris_to_string(this, &a);
-	printf("Debris_delete %s#%p.\n", a, (void *)&this->sprite.data);
-	sprite_remove(&this->sprite.data);
-	DebrisSetRemove(debris, this);
-	/* fixme: explode */
-}
-/** @implements <Debris>Action */
-static void debris_death(struct Debris *const this) {
-#if 0
-	this->hit = 0;
-	/** Spawns smaller Debris (fixme: stub.) */
-	void SpriteDebris(const struct Sprite *const s) {
-		struct AutoImage *small_image;
-		struct Sprite *sub;
-
-		if(!s || !(small_image = AutoImageSearch("AsteroidSmall.png")) || s->size <= small_image->width) return;
-
-		pedantic("SpriteDebris: %s is exploding at (%.3f, %.3f).\n", SpriteToString(s), s->x, s->y);
-
-		/* break into pieces -- new debris */
-		sub = Sprite(SP_DEBRIS, AutoDebrisSearch("SmallAsteroid"), (float)s->x, (float)s->y, s->theta * deg_to_rad, 10.0f);
-		SpriteSetVelocity(sub, deb_explosion_elasticity * s->vx, deb_explosion_elasticity * s->vy);
-	}
-	/** Enforces the maximum speed by breaking the debris into smaller chunks; the
-	 process is not elastic, so it loses energy (speed.) */
-	/*void DebrisEnforce(struct Debris *deb) {
-	 float vx, vy;
-	 float speed_2;
-
-	 if(!deb || !deb->sprite) return;
-
-	 SpriteGetVelocity(deb->sprite, &vx, &vy);
-	 if((speed_2 = vx * vx + vy * vy) > maximum_speed_2) {
-	 debug("DebrisEnforce: maximum %.3f\\,(pixels/s)^2, Deb%u is moving %.3f\\,(pixels/s)^2.\n", maximum_speed_2, DebrisGetId(deb), speed_2);
-	 DebrisExplode(deb);
-	 }
-	 }*/
-#endif
-	debris_delete(this);
-}
-/** @implements <Debris>Action */
-static void debris_action(struct Debris *const this) {
-	if(this->hit <= 0) debris_death(this);
-}
 
 /** @implements <Ship>Action */
 static void ship_out(struct Ship *const this) {
@@ -1588,65 +1433,9 @@ static void gate_action(struct Gate *const this) {
 	UNUSED(this);
 }
 
-/* define the virtual table */
-static const struct SpriteVt {
-	const SpriteAction out, update, delete;
-} debris_vt = {
-	(SpriteAction)&debris_out,
-	(SpriteAction)&debris_action,
-	(SpriteAction)&debris_delete
-}, ship_vt = {
-	(SpriteAction)&ship_out,
-	(SpriteAction)&ship_action_ai,
-	(SpriteAction)&ship_delete
-}, human_ship_vt = {
-	(SpriteAction)&ship_out,
-	(SpriteAction)&ship_action,
-	(SpriteAction)&ship_delete
-}, wmd_vt = {
-	(SpriteAction)&wmd_out,
-	(SpriteAction)&wmd_action,
-	(SpriteAction)&wmd_delete
-}, gate_vt = {
-	(SpriteAction)&gate_out,
-	(SpriteAction)&gate_action,
-	(SpriteAction)&gate_delete
-};
-
 /** @implements <Sprite>Action */
 static void sprite_update(struct Sprite *const this) {
 	this->vt->update(this);
-}
-
-/** @implements <Debris>Action */
-static void debris_filler(struct Debris *const this,
-	const struct AutoDebris *const class,
-	struct Ortho3f *const r, const struct Ortho3f *const v) {
-	unsigned bin;
-	assert(this);
-	assert(class);
-	assert(r);
-	assert(v);
-	bin = wtfaaaaailocation_to_bin(*(struct Vec2f *)r);
-	Sprite_filler(&this->sprite.data, class->sprite, bin, r, v);
-	this->sprite.data.vt = &debris_vt;
-	this->sprite.data.mass = class->mass;
-	this->hit = class->mass * debris_hit_per_mass;
-	SpriteListPush(sprites + bin, &this->sprite);
-}
-/** Constructor.
- @return Success. */
-int Debris(const struct AutoDebris *const class,
-	struct Ortho3f *r, const struct Ortho3f *v) {
-	struct Ortho3f zero = { 0.0f, 0.0f, 0.0f };
-	struct Debris *d;
-	if(!class) return 0;
-	if(!r) r = &zero;
-	if(!v) v = &zero;
-	if(!(d = DebrisSetNew(debris)))
-		{ fprintf(stderr, "Debris: %s.\n", DebrisSetGetError(debris));return 0;}
-	debris_filler(d, class, r, v);
-	return 1;
 }
 
 /** @implements <Ship>Action */
@@ -1714,168 +1503,6 @@ int Gate(const struct AutoGate *const class) {
 		{ fprintf(stderr, "Gate: %s.\n", GateSetGetError(gates)); return 0; }
 	gate_filler(gate, class);
 	return 1;
-}
-
-/** Initailises all sprite buffers.
- @return Success. */
-int Sprite(void) {
-	unsigned i;
-	if(!(bins = BinListSet())
-		|| !(debris = DebrisSet())
-		|| !(ships = ShipSet())
-		|| !(wmds = WmdSet())
-		|| !(gates = GateSet()))
-		return Sprite_(), 0;
-	BinListClear(&draw_bins);
-	BinListClear(&update_bins);
-	for(i = 0; i < BIN_BIN_SIZE; i++) SpriteListClear(sprites + i);
-	return 1;
-}
-
-/** Erases all sprite buffers. */
-void Sprite_(void) {
-	unsigned i;
-	for(i = 0; i < BIN_BIN_SIZE; i++) SpriteListClear(sprites + i);
-	BinListClear(&update_bins);
-	BinListClear(&draw_bins);
-	GateSet_(&gates);
-	WmdSet_(&wmds);
-	ShipSet_(&ships);
-	DebrisSet_(&debris);
-	BinListSet_(&bins);
-}
-
-/** Prints out debug info. */
-void SpriteOut(struct Sprite *const this) {
-	if(!this) return;
-	this->vt->out(this);
-}
-
-/** Gets a read-only copy of the position. */
-/*void SpriteGetOrtho(const struct Sprite *const this, const struct Ortho3f *ortho) {
-	if(!this) return;
-	ortho = &this->r;
-}*/
-
-/** @implements BinAction */
-static void print_bin(unsigned *bin) {
-	printf("(bin:%u)", *bin);
-}
-
-/** Choose which bins we will update/draw this frame.
- @order {O(n^2)} where {n} is the screen size */
-static void new_bins(void) {
-	static int count;
-	struct Rectangle4f rect;
-	struct Rectangle4i bin4, grow4;
-	struct Vec2i bin2i;
-	struct BinListNode *bin_node;
-	BinListClear(&draw_bins), BinListClear(&update_bins), BinListSetClear(bins);
-	DrawGetScreen(&rect);
-	Rectangle4f_to_bin4(&rect, &bin4);
-	/* the updating region extends past the drawing region */
-	Rectangle4i_assign(&grow4, &bin4);
-	if(grow4.x_min > 0)                 grow4.x_min--;
-	if(grow4.x_max + 1 < (int)bin_size) grow4.x_max++;
-	if(grow4.y_min > 0)                 grow4.y_min--;
-	if(grow4.y_max + 1 < (int)bin_size) grow4.y_max++;
-	/* draw in the centre */
-	for(bin2i.y = bin4.y_max; bin2i.y >= bin4.y_min; bin2i.y--) {
-		for(bin2i.x = bin4.x_min; bin2i.x <= bin4.y_max; bin2i.x++) {
-			if(!(bin_node = BinListSetNew(bins))){ perror("new_bins"); return; }
-			bin_node->data = bin2i_to_bin(bin2i);
-			BinListPush(&draw_bins, bin_node);
-		}
-	}
-	printf("draw in centre:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");
-#if 0
-	/* update along the outside (fixme: ugh) */
-	/*if(!(bin_node = BinListSetNew(bins))) { perror("new_bins"); return; }
-	bin_node->data = rand() / (1.0f + RAND_MAX / BIN_BIN_SIZE);
-	assert(bin_node->data < BIN_BIN_SIZE);
-	printf("*********%u\n", bin_node->data);
-	bin_node->data = 42;
-	BinListPush(&update_bins, bin_node);*/
-	if(!(bin_node = BinListSetNew(bins))){ perror("new_bins"); return; }
-	bin_node->data = 42;
-	BinListPush(&update_bins, bin_node);
-	printf("Ddraw:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");
-	if(!(bin_node = BinListSetNew(bins))){ perror("new_bins"); return; }
-	bin_node->data = 1024;
-	BinListPush(&update_bins, bin_node);
-	printf("Edraw:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");
-	if(!(bin_node = BinListSetNew(bins))){ perror("new_bins"); return; }
-	bin_node->data = 1100;
-	BinListPush(&update_bins, bin_node);
-	printf("0draw:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");
-	if((bin2i.y = grow4.y_min) < bin4.y_min) {
-		for(bin2i.x = grow4.x_min; bin2i.x <= grow4.x_max; bin2i.x++) {
-			printf("1draw:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");
-			if(!(bin_node = BinListSetNew(bins))){ perror("new_bins"); return; }
-			printf("2draw:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");
-			bin_node->data = bin2i_to_bin(bin2i);
-			printf("fyou update %u\n", bin_node->data);
-			printf("3draw:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");
-			BinListPush(&update_bins, bin_node);
-			printf("4draw:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");
-		}
-	}
-	/*printf("5draw:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");*/
-	/*printf("5update:"), BinListOrderForEach(&update_bins, &print_bin), printf("\n");*/
-#endif
-#if 0
-	if((bin2i.y = grow4.y_max) > bin4.y_max) {
-		for(bin2i.x = grow4.x_min; bin2i.x <= grow4.x_max; bin2i.x++) {
-			if(!(bin_node = BinListSetNew(bins))){ perror("new_bins"); return; }
-			bin_node->data = bin2i_to_bin(bin2i);
-			BinListPush(&update_bins, bin_node);
-		}
-	}
-	if((bin2i.x = grow4.x_min) < bin4.x_min) {
-		for(bin2i.y = bin4.y_min; bin2i.y <= bin4.y_max; bin2i.y++) {
-			if(!(bin_node = BinListSetNew(bins))){ perror("new_bins"); return; }
-			bin_node->data = bin2i_to_bin(bin2i);
-			BinListPush(&update_bins, bin_node);
-		}
-	}
-	if((bin2i.x = grow4.x_max) > bin4.x_max) {
-		for(bin2i.y = bin4.y_min; bin2i.y <= bin4.y_max; bin2i.y++) {
-			if(!(bin_node = BinListSetNew(bins))){ perror("new_bins"); return; }
-			bin_node->data = bin2i_to_bin(bin2i);
-			BinListPush(&update_bins, bin_node);
-		}
-	}
-#endif
-	if(!count) {
-		printf("*** get screen: (x:%.3f, %.3f)(y:%.3f, %.3f).\n", rect.x_min, rect.x_max, rect.y_min, rect.y_max);
-		printf("*** get bin: (x:%d, %d)(y:%d, %d).\n", bin4.x_min, bin4.x_max, bin4.y_min, bin4.y_max);
-		printf("draw:"), BinListOrderForEach(&draw_bins, &print_bin), printf("\n");
-		printf("update:"), BinListOrderForEach(&update_bins, &print_bin), printf("\n");
-	}
-	count++, count &= 65535;
-}
-/** {{act} \in { Draw }}. */
-static void for_each_draw(const SpriteAction act) {
-	struct BinListNode *iter;
-	unsigned bin;
-	assert(act);
-	for(iter = BinListOrderGetFirst(&draw_bins); iter;
-		iter = BinListNodeOrderGetNext(iter)) {
-		bin = iter->data;
-		SpriteListXForEach(sprites + bin, act);
-	}
-}
-/** {{act} \in { Update, Draw }}. */
-static void for_each_update(const SpriteAction act) {
-	struct BinListNode *iter;
-	unsigned bin;
-	assert(act);
-	for(iter = BinListOrderGetFirst(&update_bins); iter;
-		iter = BinListNodeOrderGetNext(iter)) {
-		bin = iter->data;
-		SpriteListXForEach(sprites + bin, act);
-	}
-	for_each_draw(act);
 }
 
 /* Compute bounding boxes of where the sprite wants to go this frame,
@@ -2033,65 +1660,6 @@ static int collide_circles(const struct Vec2f a, const struct Vec2f b,
 	if(t < 0.0f) t = 0.0f; /* it can not be > 1 because dist < r^2 */
 	*t0_ptr = t;
 	return 1;
-}
-
-
-/** This is where most of the work gets done. Called every frame, O(n).
- @param frame_ms: Milliseconds since the last frame. */
-void SpriteUpdate(const int frame_ms) {
-	/* going to use it a lot to scale the time evolution: put in in a static */
-	dt_ms = frame_ms;
-	/* set up the bins according to the camera; do not move the camera after
-	 this in the frame */
-	new_bins();
-	/* collision detection */
-	/*for_each_update(&collide_extrapolate);
-	for_each_update(&collide_boxes);*/
-	/* sort the sprites */
-	/* do some stuff */
-	for_each_update(&sprite_update);
-}
-
-/** @fixme C99
- @return The first sprite in the {bin2}. */
-inline const struct SpriteList *SpriteListBin(struct Vec2u bin2) {
-	return sprites + bin2_to_bin(bin2);
-}
-
-/** @fixme C99 */
-inline void SpriteExtractDrawing(const struct Sprite *const this,
-	const struct Ortho3f **const r_ptr, unsigned *const image_ptr,
-	unsigned *const normals_ptr, const struct Vec2u **const size_ptr) {
-	assert(this);
-	assert(r_ptr && image_ptr && normals_ptr && size_ptr);
-	*r_ptr       = (struct Ortho3f *)&this->r;
-	*image_ptr   = this->image;
-	*normals_ptr = this->normals;
-	*size_ptr    = &this->size;
-}
-
-void ShipShoot(struct Ship *const this) {
-	struct Wmd *wmd;
-	assert(this);
-	if(!TimerIsTime(this->ms_recharge_wmd)) return;
-	if(!(wmd = WmdSetNew(wmds)))
-		{ fprintf(stderr, "Wmd: %s.\n", WmdSetGetError(wmds)); return; }
-	wmd_filler(wmd, this->class->weapon, this);
-	this->ms_recharge_wmd = TimerGetGameTime()
-		+ this->class->weapon->ms_recharge;
-}
-
-void ShipGetPosition(const struct Ship *this, struct Vec2f *pos) {
-	if(!this || !pos) return;
-	pos->x = this->sprite.data.r.x;
-	pos->y = this->sprite.data.r.y;
-}
-
-void ShipSetPosition(struct Ship *const this, struct Vec2f *const pos) {
-	if(!this || !pos) return;
-	this->sprite.data.r.x = pos->x;
-	this->sprite.data.r.y = pos->y;
-	/* fixme: update bin */
 }
 
 /** @implements <Ship>Action */
